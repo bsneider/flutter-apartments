8d3ac1dfa5484e71022855d57ef7453deda1312f
Brandon Sneider <54788551+bsneider@users.noreply.github.com>
2020-04-18 02:38:42 -0500

    scalable image

--- flutter_apartments/lib/screens/welcome_screen.dart
+++ flutter_apartments/lib/screens/welcome_screen.dart
@@ -3,156 +3,77 @@ import 'package:speech_bubble/speech_bubble.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import '../ui_widgets/size_config.dart';
 import 'package:align_positioned/align_positioned.dart';
-
-class Demo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    //
-    // Center widget A, and then align B to the bottom of that.
-    return Scaffold(
-      appBar: AppBar(title: const Text('Center Below Example')),
-      body: Center(
-        child: AlignPositioned.relative(widgetA(), widgetB(),
-            moveByContainerHeight: 0.5,
-            moveByChildHeight: 0.5,
-            rotateDegrees: 10),
-      ),
-    );
-  }
-
-  Container widgetA() => Container(
-        color: Colors.red,
-        child: Text(
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
-            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
-            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi "
-            "ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit "
-            "in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 25)),
-      );
-
-  Container widgetB() => Container(
-      color: Colors.blue,
-      child: Text(
-          "Excepteur sint occaecat.\n"
-          "Duis aute irure dolor in reprehenderit.",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 16)));
-}
+import 'dart:math';
 
 class ValuePropImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // return Demo();
     SizeConfig().init(context);
     var fontSize = SizeConfig.safeBlockHorizontal * 15;
-    return Expanded(
-        child: Stack(
+    return Scaffold(
+        body: Container(
+            child: Stack(
       fit: StackFit.expand,
       children: <Widget>[
-        AlignPositioned.relative(
-            Icon(
-              Icons.people,
-              size: fontSize,
-            ),
-            Padding(
-                padding: EdgeInsets.fromLTRB(fontSize / 2.5, 0, 0, 0),
-                child: Align(
-                  child: buildSpeechBubble(context),
-                  alignment: Alignment.topLeft,
-                )),
-            moveByContainerHeight: -.3),
-        Positioned(
-            left: fontSize,
-            child: Icon(
-              Icons.add,
-              size: fontSize,
+        Padding(
+            padding: EdgeInsets.fromLTRB(fontSize / 2.5, 0, 0, 0),
+            child: Align(
+              child: buildSpeechBubble(context),
+              alignment: Alignment.topLeft,
             )),
-        AlignPositioned.relative(
-            Positioned(
-                left: fontSize * 2,
+        Padding(
+            padding: EdgeInsets.fromLTRB(0, pow(fontSize + 1000, 0.5), 0, 0),
+            child: Align(
+                child: Icon(
+                  Icons.people,
+                  size: fontSize,
+                ),
+                alignment: Alignment.topLeft)),
+        Padding(
+            padding:
+                EdgeInsets.fromLTRB(fontSize, pow(fontSize + 1000, 0.5), 0, 0),
+            child: Align(
+                child: Icon(
+                  Icons.add,
+                  size: fontSize,
+                ),
+                alignment: Alignment.topLeft)),
+        Padding(
+            padding: EdgeInsets.fromLTRB(
+                fontSize * 2, pow(fontSize + 1000, 0.5), 0, 0),
+            child: Align(
                 child: Icon(
                   Icons.home,
                   size: fontSize,
-                )),
-            buildHouseSpeechBubble(context, fontSize),
-            moveByContainerHeight: .09,
-            moveByContainerWidth: .12),
-        Positioned(
-            left: fontSize * 3,
-            child: Icon(
-              Icons.drag_handle,
-              size: fontSize,
-            )),
-        Positioned(
-            left: fontSize * 4,
-            child: AutoSizeText(
-              '\$\$\$',
-              style: TextStyle(
-                  fontSize: fontSize,
-                  color: Theme.of(context).textTheme.bodyText2.color),
-            )),
+                ),
+                alignment: Alignment.topLeft)),
+        Padding(
+            padding: EdgeInsets.fromLTRB(
+                fontSize * 3, pow(fontSize + 1000, 0.5), 0, 0),
+            child: Align(
+                child: Icon(
+                  Icons.drag_handle,
+                  size: fontSize,
+                ),
+                alignment: Alignment.topLeft)),
+        Padding(
+            padding: EdgeInsets.fromLTRB(
+                fontSize * 4, pow(fontSize + 1000, 0.5), 0, 0),
+            child: Align(
+                child: AutoSizeText(
+                  '\$\$\$',
+                  style: TextStyle(
+                      fontSize: fontSize,
+                      color: Theme.of(context).textTheme.bodyText2.color),
+                ),
+                alignment: Alignment.topLeft)),
+        buildHouseSpeechBubble(context, fontSize),
       ],
-    ));
-    // return Column(children: <Widget>[
-    //   AlignPositioned.relative(
-    //       AlignPositioned.relative(buildEquation(fontSize, context),
-    //           buildHouseSpeechBubble(context, fontSize),
-    //           moveByContainerHeight: 1.0),
-    //       buildSpeechBubble(context),
-    //       moveByContainerHeight: -0.8)
-    // ]);
-  }
-
-  Row buildEquation(double fontSize, BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Row(children: <Widget>[
-          Icon(
-            Icons.people,
-            size: fontSize,
-          ),
-          Icon(
-            Icons.add,
-            size: fontSize,
-          ),
-          // AlignPositioned.relative(
-          Icon(
-            Icons.home,
-            size: fontSize,
-          ),
-          // buildHouseSpeechBubble(context, fontSize),
-          // moveByContainerHeight: 0.5,
-          // moveByChildHeight: 2.5),
-
-          Icon(Icons.drag_handle, size: fontSize),
-          AutoSizeText(
-            '\$\$\$',
-            style: TextStyle(
-                fontSize: fontSize,
-                color: Theme.of(context).textTheme.bodyText2.color),
-          )
-        ]),
-      ],
-    );
+    )));
   }
 
   Row buildHouseSpeechBubble(BuildContext context, double fontSize) {
     return Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-      // Opacity(
-      //     opacity: 0.0,
-      //     child: Row(children: <Widget>[
-      //       Icon(
-      //         Icons.people,
-      //         size: fontSize,
-      //       ),
-      //       Icon(
-      //         Icons.add,
-      //         size: fontSize,
-      //       ),
-      //     ])),
       SpeechBubble(
         nipLocation: NipLocation.TOP_LEFT,
         padding: EdgeInsets.all(10.0),
@@ -200,12 +121,7 @@ class WeclomeScreen extends StatelessWidget {
           backgroundColor: Colors.white,
           centerTitle: true,
         ),
-        body: Column(
-          children: <Widget>[
-            AlignPositioned.relative(titleSection, ValuePropImage(),
-                moveByContainerHeight: 1.1)
-          ],
-        ));
+        body: ValuePropImage());
   }
 }
 
