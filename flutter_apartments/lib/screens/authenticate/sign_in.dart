a8565252f83e3580cd55a1b9b31150dd7ace3051
Brandon Sneider <54788551+bsneider@users.noreply.github.com>
2020-05-02 15:37:45 -0500

    add spinning widget
    a few name fiixes

--- flutter_apartments/lib/screens/authenticate/sign_in.dart
+++ flutter_apartments/lib/screens/authenticate/sign_in.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_apartments/services/auth.dart';
 import 'package:email_validator/email_validator.dart';
+import 'package:flutter_apartments/common/contants.dart';
+import 'package:flutter_apartments/common/loading.dart';
 
 class SignIn extends StatefulWidget {
   final Function toggleView;
@@ -12,72 +14,79 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
   // text field state
   String email = '';
   String password = '';
   String error = '';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign in to Mil To Mil'),
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () {
-                widget.toggleView();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Register'))
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 20.0),
-                TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(hintText: 'email'),
-                    validator: (val) => EmailValidator.validate(val)
-                        ? null
-                        : 'Enter a valid email',
-                    onChanged: (val) {
-                      setState(() => email = val);
-                    }),
-                SizedBox(height: 20.0),
-                TextFormField(
-                    decoration: const InputDecoration(hintText: 'password'),
-                    validator: (val) => val.length < 6
-                        ? 'Enter a password 6+ chars long'
-                        : null,
-                    obscureText: true,
-                    onChanged: (val) {
-                      setState(() => password = val);
-                    }),
-                SizedBox(height: 20.0),
-                RaisedButton(
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        dynamic result = await _auth.signInWithEmailAndPassword(
-                            email, password);
-                        if (result == null) {
-                          setState(() => error = 'could not sign in with those credentials');
-                        }
-                        print(email);
-                        print(password);
-                      }
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sign in to Mil To Mil'),
+              actions: <Widget>[
+                FlatButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
                     },
-                    color: Colors.pink[400],
-                    child:
-                        Text('Register', style: TextStyle(color: Colors.white)))
+                    icon: Icon(Icons.person),
+                    label: Text('Register'))
               ],
-            )),
-      ),
-    );
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: textInputDecoration,
+                          validator: (val) => EmailValidator.validate(val)
+                              ? null
+                              : 'Enter a valid email',
+                          onChanged: (val) {
+                            setState(() => email = val);
+                          }),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              hintText: 'Password'),
+                          validator: (val) => val.length < 6
+                              ? 'Enter a password 6+ chars long'
+                              : null,
+                          obscureText: true,
+                          onChanged: (val) {
+                            setState(() => password = val);
+                          }),
+                      SizedBox(height: 20.0),
+                      RaisedButton(
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              setState(() => loading = true);
+                              dynamic result = await _auth
+                                  .signInWithEmailAndPassword(email, password);
+                              if (result == null) {
+                                setState(() {
+                                  error =
+                                      'could not sign in with those credentials';
+                                  loading = false;
+                                });
+                              }
+                            }
+                          },
+                          color: Colors.pink[400],
+                          child: Text('Sign in',
+                              style: TextStyle(color: Colors.white)))
+                    ],
+                  )),
+            ),
+          );
   }
 }
